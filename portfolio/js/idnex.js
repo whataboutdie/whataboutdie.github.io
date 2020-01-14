b98b1940ef01790c2a5a1d250920166f1eb06616
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-01-14 22:18:27 +0200

    projects menu added

--- portfolio/js/idnex.js
+++ portfolio/js/idnex.js
@@ -6,9 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let chosenMenu = document.getElementById('show_chosen_menu'),
         menuAbout = document.getElementById('menu_about'),
         menuProjects = document.getElementById('menu_projects'),
+        showProjects = document.getElementById('show_projects'),
         wrapper = document.getElementById('wrapper'),
         pleaseholder = document.getElementById('viewport-small'),
-        viewportWidth = document.documentElement.clientWidth;
+        viewportWidth = document.documentElement.clientWidth,
+        timer;
 
     
     if (viewportWidth <= 1000) {
@@ -38,5 +40,33 @@ document.addEventListener('DOMContentLoaded', () => {
         chosenMenu.innerHTML = '';
         chosenMenu.classList.remove('opacity');
     })
+
+    /**********/
+
+    menuProjects.addEventListener('click' , () =>{
+        if (showProjects.style.display == 'block') {
+            showProjects.classList.add('close');
+            showProjects.classList.remove('open');
+            console.log('closed');
+            setTimeout(closeitfuckyou, 500);
+            menuProjects.innerHTML = 'projects'
+            menuProjects.style.background = 'white';
+            menuProjects.style.color = 'black';
+                  
+        } else {
+            showProjects.style.display = 'block';
+            showProjects.classList.add('open');
+            showProjects.classList.remove('close');
+            console.log('opened');
+            menuProjects.innerHTML = 'close';
+            menuProjects.style.background = 'black';
+            menuProjects.style.color = 'white';
+        }
+        
+    });
+
+    function closeitfuckyou(params) {
+        showProjects.style.display = 'none';
+    }
 
 });
