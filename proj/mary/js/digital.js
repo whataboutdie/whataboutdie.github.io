51ebed3649e247bba323fd1e50591bfc2c5483f6
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-02-15 22:18:48 +0200

    mob ver3

--- proj/mary/js/digital.js
+++ proj/mary/js/digital.js
@@ -1,49 +1,57 @@
 'use strict';
+
+// cheking whether touch screen on devise
+// however, i should say thanks for capability of using free touch screen detector was made by https://twitter.com/modernizr !   
+
+
+let Wrapper = document.getElementById('wrapper');
+
+
+function is_touch_device() {
+
+    var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+    
+    var mq = function (query) {
+        return window.matchMedia(query).matches;
+    }
+
+    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+        return true;
+    }
+
+    var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+    return mq(query);
+}
+
+if (is_touch_device() === false) {
+    console.log('FALSE! device does not have `touch screen`');
+    Wrapper.classList.add('wrapper');
+    Wrapper.setAttribute('data-0', 'transform: translateX(0%)');
+    Wrapper.setAttribute('data-2000', 'transform: translateX(-100%)');
+    
+
+    
+} else {
+    document.querySelector('body').style.overflow = 'visible !important';
+    document.querySelector('html').style.overflow = 'visible !important';
+    Wrapper.classList.add('wrapper-new');
+
+    console.log('TRUE! device has `touch screen`');    
+    
+
+}
+
+// when DOM`s finally loaded:
+
 document.addEventListener('DOMContentLoaded', () =>{
     console.log('welcome to my console!');
 
     let HeaderMovingBtn = document.getElementById('header__button'),
         SectionSheet = document.getElementById('sheet__body'),
         ListOffBtn = document.getElementById('sheet-list-off'),
-        ListOnBtn = document.getElementById('sheet-list-on'),
-        Wrapper = document.getElementById('wrapper'),
-        SkrollrJs = document.getElementById("skrollr");
+        ListOnBtn = document.getElementById('sheet-list-on');
 
     let targetPos;
-
-    // way to detect a 'touch screen' device
-    function is_touch_device() {
-
-        var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-        
-        var mq = function (query) {
-            return window.matchMedia(query).matches;
-        }
-    
-        if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-            return true;
-        }
-    
-        var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-        return mq(query);
-    }
-
-    if (is_touch_device() == false) {
-        
-        console.log('false');
-
-        
-
-    } else {
-        console.log('true');
-        Wrapper.classList.add('wrapper-new');
-        Wrapper.classList.remove('wrapper');
-        // Remove the child element from the document
-        SkrollrJs.node.remove();
-
-        
-        
-    }
 
     function scrollToSection(target) { //func describes the way it moves down to necessary section(target)
         targetPos = target.getBoundingClientRect().right;
@@ -68,8 +76,4 @@ document.addEventListener('DOMContentLoaded', () =>{
         ListOffBtn.classList.toggle('active__list');
         ListOnBtn.classList.toggle('active__list');
     });
-    
-
-
-    
 });
