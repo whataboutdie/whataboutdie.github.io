fe42eee57d785a401270baf8f8a11003d6a5ef1c
whataboutdie <55838310+whataboutdie@users.noreply.github.com>
2020-02-15 19:16:55 +0200

    mobile adaptation ver1

--- proj/mary/js/btn.js
+++ proj/mary/js/btn.js
@@ -15,16 +15,4 @@ $(document).ready(function (){
         $('.label__modal_menu').removeClass('label__modal_menu-open');
     });
     
-    // Digital!
-    
-    $('#sheet-list-off').on('click', function(e){
-        $('.sheet__list-off').toggleClass('active__list');
-        $('.sheet__list-on').toggleClass('active__list');
-    });
-    
-    $('#sheet-list-on').on('click', function(e){
-        $('.sheet__list-off').toggleClass('active__list');
-        $('.sheet__list-on').toggleClass('active__list');
-    });
-
 });
--- proj/mary/js/digital.js
+++ proj/mary/js/digital.js
@@ -3,34 +3,71 @@ document.addEventListener('DOMContentLoaded', () =>{
     console.log('welcome to my console!');
 
     let HeaderMovingBtn = document.getElementById('header__button'),
-        SectionSheet = document.getElementById('sheet__body');
+        SectionSheet = document.getElementById('sheet__body'),
+        ListOffBtn = document.getElementById('sheet-list-off'),
+        ListOnBtn = document.getElementById('sheet-list-on'),
+        Wrapper = document.getElementById('wrapper'),
+        SkrollrJs = document.getElementById("skrollr-js");
 
-    let targetPos,
-        buttonPos;
+    let targetPos;
 
-    /*window.addEventListener('scroll' , () =>{
-        let scrolled = window.scrollY;
-        console.log(scrolled);
-    });*/
+    // way to detect a 'touch screen' device
+    function is_touch_device() {
+
+        var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+        
+        var mq = function (query) {
+            return window.matchMedia(query).matches;
+        }
+    
+        if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+            return true;
+        }
+    
+        var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+        return mq(query);
+    }
+
+
+    //
+    if (is_touch_device() === true) {
+        console.log('true');
+
+        // Remove the child element from the document
+        SkrollrJs.remove();
+
+
+        Wrapper.classList.remove('wrapper');
+        Wrapper.classList.add('wrapper-touch-true');
+
+    } else {
+        console.log('false');
+    }
 
     function scrollToSection(target, button) { //func describes the way it moves down to necessary section(target)
         targetPos = target.getBoundingClientRect().right;
-        buttonPos = button.getBoundingClientRect();
-
-        console.log('buttonPos:',  buttonPos);
         console.log('tragetPos:',  targetPos);
+        window.scrollTo({
+        top: targetPos,
+        behavior: 'smooth'
+        });
 
-            window.scrollTo({
-            top: targetPos,
-            behavior: 'smooth'
-            });
-
-            button.scrollLeft = targetPos;
     };
 
     HeaderMovingBtn.addEventListener('click', () =>{
         scrollToSection(SectionSheet, HeaderMovingBtn);
     });
+
+    ListOffBtn.addEventListener('click', ()=>{
+        ListOffBtn.classList.toggle('active__list');
+        ListOnBtn.classList.toggle('active__list');
+    });
+
+    ListOnBtn.addEventListener('click', ()=>{
+        ListOffBtn.classList.toggle('active__list');
+        ListOnBtn.classList.toggle('active__list');
+    });
+
 
     
 });
